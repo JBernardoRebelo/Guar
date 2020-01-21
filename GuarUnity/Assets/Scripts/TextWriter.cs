2ec1e6b333701cd9a3aabc7155c0175013fc5050
JBernardoRebelo <bernasfasttaker@gmail.com>
2020-01-21 16:27:26 +0000

    Add backstory unity

---
+++ GuarUnity/Assets/Scripts/TextWriter.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextWriter : MonoBehaviour
+{
+    //[SerializeField] private static string _path = "Assets/Texts/BackStory_1.txt";
+    //private StreamReader _file = new StreamReader(_path, true);
+
+    [SerializeField] private float _wait = 0.1f;
+    [SerializeField] private string _fullText;
+
+    private void Start()
+    {
+        StartCoroutine(OutputText());
+    }
+
+    // Converts the file into separate lines and into separate chars to output
+    private IEnumerator OutputText()
+    {
+        string currentText = "";
+
+        for (int i = 0; i <= _fullText.Length; i++)
+        {
+            currentText = _fullText.Substring(0, i);
+
+            GetComponent<Text>().text = currentText;
+
+            yield return new WaitForSeconds(_wait);
+        }
+    }
+
+}
