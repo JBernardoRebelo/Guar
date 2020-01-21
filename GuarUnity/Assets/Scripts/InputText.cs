9ad42dede4aee78b24bba9de564dbc866f3530c3
JBernardoRebelo <bernasfasttaker@gmail.com>
2020-01-21 17:50:43 +0000

    Add button

---
+++ GuarUnity/Assets/Scripts/InputText.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class InputText : MonoBehaviour
+{
+    private string _inputedText;
+
+    [SerializeField] private GameObject _inputField;
+    [SerializeField] private GameObject _textDisplay;
+
+    public void StoreName()
+    {
+        _inputedText = _inputField.GetComponent<Text>().text;
+        _textDisplay.GetComponent<Text>().text = "merda";
+    }
+
+}
